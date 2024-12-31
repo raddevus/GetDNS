c6439c7dea24a3b8b7d92ad75501aed97af35f2a
raddevus <1radeutsch1@gmail.com>
2024-12-31 17:36:35 -0500

    Proj to help me research issue with DNS & local network showing error when attempting to load lowes.com

---
+++ Program.cs
@@ -0,0 +1,27 @@
+﻿//GetDNS
+// I'm using this code in an attempt to discover what the issue is
+// where my network refuses to load lowes.com
+using System.Net;
+using System.Net.NetworkInformation;
+using System.Security.Cryptography.X509Certificates;
+
+class Program
+{
+    static void Main()
+    {
+        foreach (NetworkInterface ni in NetworkInterface.GetAllNetworkInterfaces())
+        {
+            
+            if (ni.OperationalStatus == OperationalStatus.Up)
+            {
+                IPInterfaceProperties ipProps = ni.GetIPProperties();
+                foreach (IPAddress dns in ipProps.DnsAddresses)
+                {
+                    Console.WriteLine($"network interface: {ni.Name} - DNS Server: {dns}");
+                }
+                
+            }
+        }
+    }
+}
+
